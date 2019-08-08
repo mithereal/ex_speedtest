a12f2a35125bd3bdc44bfafe0fb809482e1a4103
Jason Clark <mithereal@gmail.com>
2019-08-07 18:03:41 -0700

    add license

--- mix.exs
+++ mix.exs
@@ -6,8 +6,13 @@ defmodule Speedtest.MixProject do
       app: :speedtest,
       version: "0.1.0",
       elixir: "~> 1.8",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Speedtest",
+      source_url: "https://github.com/elixir-ecto/postgrex"
     ]
   end
 
@@ -23,9 +28,26 @@ defmodule Speedtest.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
       {:httpoison, "~> 1.5"},
       {:sweet_xml, "~> 0.6.6"},
       {:geocalc, "~> 0.5"}
+    ]
+  end
+
+  defp description() do
+    "A Speedtest Client for Elixir"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "speedtest",
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/mithereal/speedtest"}
     ]
   end
 end
